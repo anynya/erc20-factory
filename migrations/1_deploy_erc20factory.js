1a7f2c3ba529318d5c171ec10f36f8fe95dc2002
anynya <39265154+anynya@users.noreply.github.com>
2023-04-26 11:08:05 +0800

    Update 1_deploy_erc20factory.js

--- migrations/1_deploy_erc20factory.js
+++ migrations/1_deploy_erc20factory.js
@@ -3,20 +3,15 @@ const ERC20Template = artifacts.require("ERC20Template");
 const ERC20Factory = artifacts.require("ERC20Factory");
 const MintableERC20Template = artifacts.require("InitializableMintableERC20");
 
-
-
 module.exports = async (deployer, network, accounts) => {
     await deployer.deploy(CloneFactory);
     CloneFactoryAddress = CloneFactory.address;
-    logger.log("CloneFactoryAddress: ", CloneFactoryAddress);
 
     await deployer.deploy(ERC20Template);
     ERC20TemplateAddress = ERC20Template.address;
-    logger.log("ERC20TemplateAddress: ", ERC20TemplateAddress);
 
     await deployer.deploy(MintableERC20Template);
     MintableERC20TemplateAddress = MintableERC20Template.address;
-    logger.log("MintableERC20TemplateAddress: ", MintableERC20TemplateAddress);
 
     await deployer.deploy(
         ERC20Factory,
@@ -25,5 +20,4 @@ module.exports = async (deployer, network, accounts) => {
         MintableERC20TemplateAddress
     );
     ERC20FactoryAddress = ERC20Factory.address;
-    logger.log("ERC20FactoryAddress: ", ERC20FactoryAddress);
 }
